55d3bcdeb18625142588d6f6404452ea02c5b440
hp <pratikshagadkari123@gmail.com>
2019-06-06 23:00:41 +0530

    code

---
+++ app/src/main/java/com/example/hp/vegetoes/Offer.java
@@ -0,0 +1,30 @@
+package com.example.hp.vegetoes;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hp.vegetoes.Model.Products;
+import com.example.hp.vegetoes.ViewHolder.ProductViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+
+public class Offer extends AppCompatActivity {
+    private DatabaseReference ProductsRef;
+    private RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_offer);
+
+
+    }
+}
